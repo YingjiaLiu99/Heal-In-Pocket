3cdea61432d7f917775449fbcc511c460e0fae08
YingjiaLiu99 <yliu4033@gmail.com>
2024-08-08 23:17:28 -0500

    upper nav bar finished

--- src/app/ui/provider/sidenav.tsx
+++ src/app/ui/provider/sidenav.tsx
@@ -1,18 +1,24 @@
 import styles from './sidenav.module.css';
+import Image from 'next/image';
 
 export default function SideNavigator() {
     return (
         <div className={styles.header}>
             <div className={styles.leftSection}>
-                logo place
+                <Image src="/HIP_logo_row.png" width={320} height={90} alt="logo image" />
             </div>
 
-            <div className={styles.middleSection}>
-                empty
+            <div className={styles.middleSection}>               
             </div>
 
             <div className={styles.rightSection}>
-                login&user
+                <div className={styles.notificationIcon}>
+                    <Image src="/notifications.svg" width={40} height={40} alt="notifications Icon" />
+                </div>
+                
+                <div className={styles.userIcon}>
+                    user
+                </div>
             </div>            
         </div>
     );
