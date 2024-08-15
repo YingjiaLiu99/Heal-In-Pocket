e6da4dd9b22c439895bd57eba47c32ff49d2c526
YingjiaLiu99 <yliu4033@gmail.com>
2024-08-14 23:33:22 -0500

    feat: login UI design and basics

---
+++ src/app/(login&signup)/login/page.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import { useState } from 'react';
+import styles from './page.module.css';
+
+export default function Login() {
+  const [account, setAccount] = useState('');
+  const [password, setPassword] = useState('');
+  const [location, setLocation] = useState('');
+  const [userType, setUserType] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = () => {
+    console.log(`Password: ${password}, Location: ${location}, UserType: ${userType}`);
+    console.log('submitted');
+  };
+
+  return (
+    <main>
+      <div className={styles.main}>
+        <div className={styles.textContainer}>
+          <h1>Welcome back!</h1>
+          <h2>We are excited to see you again!</h2>
+        </div>
+        <div className={styles.container}>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="account" className={styles.label}>
+              EMAIL OR PHONE NUMBER
+            </label>
+            <input
+              type="text"
+              id="account"
+              name="account"
+              placeholder=""
+              value={account}
+              onChange={(e) => setAccount(e.target.value)}
+              className={styles.input}
+            />
+
+            <label htmlFor="password" className={styles.label}>
+              PASSWORD
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder=""
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className={styles.input}
+            />
+
+            <label htmlFor="userType" className={styles.label}>
+              User
+            </label>
+            <select
+              id="userType"
+              name="userType"
+              value={userType}
+              onChange={(e) => setUserType(e.target.value)}
+              className={styles.select}
+            >
+              <option value="" disabled hidden>
+                Select user type
+              </option>
+              <option value="patient">Patient</option>
+              <option value="provider">Provider</option>
+              <option value="volunteer">Volunteer</option>
+            </select>
+
+            <label htmlFor="location" className={styles.label}>
+              LOCATION
+            </label>
+            <select
+              id="location"
+              name="location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              className={styles.select}
+            >
+              <option value="Street Corner Care">Street Corner Care, San Diego</option>
+            </select>
+
+            <input type="submit" value="Submit" className={styles.submit} />
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/app/demo/provider/(home)/page.tsx
+++ src/app/demo/provider/(home)/page.tsx
@@ -2,8 +2,8 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.main}>
+    <main className={styles.main}>
       <p>This is provider home!</p>
-    </div>
+    </main>
   );
 }
