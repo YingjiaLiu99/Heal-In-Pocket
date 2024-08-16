7a7b9bb5bd15b5a1d0703a767364efbfd2ad6705
YingjiaLiu99 <yliu4033@gmail.com>
2024-08-16 11:34:37 -0500

    fix: text invisible on mobile browser, image loading issue

--- src/app/ui/intro/header.tsx
+++ src/app/ui/intro/header.tsx
@@ -7,41 +7,26 @@ export default function Header() {
     <div className={styles.header}>
       <div className={styles.leftSection}>
         <Link href="/" style={{ display: 'flex' }}>
-          <Image
-            src="/HIP_logo_row.png"
-            width={249}
-            height={70}
-            priority
-            alt="logo"
-            className="hidden sm:block"
-          />
-          <Image
-            src="/pure_logo.jpg"
-            width={70}
-            height={70}
-            priority
-            alt="logo"
-            className="block sm:hidden"
-          />
+          <Image src="/HIP_logo_row.png" width={249} height={70} priority alt="logo" />
         </Link>
       </div>
 
       <div className={styles.middleSection}>
-        <div className={styles.menu}>
-          <Link href="/about">About Us</Link>
-        </div>
-        <div className={styles.menu}>
-          <Link href="/projects">Projects</Link>
-        </div>
-        <div className={styles.menu}>
-          <Link href="/get-involved">Get Involved</Link>
-        </div>
+        <Link href="/about">
+          <div className={styles.menu}>About Us</div>
+        </Link>
+        <Link href="/projects">
+          <div className={styles.menu}>Projects</div>
+        </Link>
+        <Link href="/get-involved">
+          <div className={styles.menu}>Get Involved</div>
+        </Link>
       </div>
 
       <div className={styles.rightSection}>
-        <div className={styles.openButton}>
-          <Link href="/login">Open Heal in Pocket</Link>
-        </div>
+        <Link href="/login">
+          <div className={styles.openButton}>Open Heal in Pocket</div>
+        </Link>
       </div>
     </div>
   );
