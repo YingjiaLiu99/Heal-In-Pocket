f789bffe167a735819074265dbcbd2de65d36c0b
YingjiaLiu99 <yliu4033@gmail.com>
2024-08-26 22:57:07 -0500

    feat: popup record form

--- src/app/demo/provider/(home)/recordPopup.tsx
+++ src/app/demo/provider/(home)/recordPopup.tsx
@@ -12,7 +12,6 @@ const formatDate = (date: Date): string => {
   const month = String(date.getMonth() + 1).padStart(2, '0'); // getMonth() returns 0-based index
   const day = String(date.getDate()).padStart(2, '0');
   const year = date.getFullYear();
-
   return `${month}/${day}/${year}`;
 };
 
@@ -45,7 +44,56 @@ export default function RecordPopupWindow({ patient, record, handleClose }: reco
 
         <div className={styles.contentContainer}>
           <h1>Visit Note</h1>
-          <div></div>
+          <div className={styles.inputBoxContainer}>
+            <label htmlFor="chiefComplaint" className={styles.label}>
+              Chief Complaint
+            </label>
+            <textarea
+              id="chiefComplaint"
+              name="chiefComplaint"
+              placeholder=""
+              className={styles.inputBox}
+              rows={2}
+            />
+          </div>
+
+          <div className={styles.inputBoxContainer}>
+            <label htmlFor="subjective" className={styles.label}>
+              Subjective
+            </label>
+            <textarea
+              id="subjective"
+              name="subjective"
+              placeholder=""
+              className={styles.inputBox}
+              rows={4}
+            />
+          </div>
+          <div className={styles.inputBoxContainer} style={{ backgroundColor: 'lightyellow' }}>
+            <label htmlFor="medicalHistory" className={styles.label}>
+              Medical History
+            </label>
+            <textarea
+              id="medicalHistory"
+              name="medicalHistory"
+              placeholder=""
+              className={styles.inputBox}
+              rows={2}
+              style={{ backgroundColor: 'lightyellow' }}
+            />
+          </div>
+          <div className={styles.inputBoxContainer}>
+            <label htmlFor="med/allergy" className={styles.label}>
+              Current Medication/Allergies
+            </label>
+            <textarea
+              id="med/allergy"
+              name="med/allergy"
+              placeholder=""
+              className={styles.inputBox}
+              rows={2}
+            />
+          </div>
         </div>
       </div>
     </div>
